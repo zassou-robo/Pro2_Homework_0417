16be50a73f31e480a2fb43811eb6cb343fe491c1
zassou-robo <zassou.sub@gmail.com>
2026-04-30 17:04:04 +0900

    演習3を追加

--- 03_03_homework.cpp
+++ 03_03_homework.cpp
@@ -3,3 +3,16 @@ using namespace std;
 constexpr int APPLE_PRICE = 100;
 constexpr int ORANGE_PRICE = 88;
 constexpr int STRAWBERRY_PRICE = 398;
+
+int apple_amount = 0;
+int orange_amount = 0;
+int strawberry_amount = 0;
+
+int main()
+{
+  scanf("%d\n", &apple_amount);
+  scanf("%d\n", &orange_amount);
+  scanf("%d\n", &strawberry_amount);
+  int all_price = apple_amount * APPLE_PRICE + orange_amount * ORANGE_PRICE + strawberry_amount * STRAWBERRY_PRICE;
+  printf("%d", all_price);
+}
